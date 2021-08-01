be3edc88c9f7c50a57f234f8c5230c70f04eb27d
KaroHovhannisyan <kar.hovhannisyan.95@gmail.com>
2021-08-02 02:49:13 +0400

    Added tests

--- src/__tests__/Button.test.tsx
+++ src/__tests__/Button.test.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import { Button } from '../common/components';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { Button } from "../common/components";
 
-it('Should render button text and trigger onclick', () => {
-  const onClick = jest.fn();  
+it("Should render button text and trigger onclick", () => {
+  const onClick = jest.fn();
   const button = render(<Button text="test" onClick={onClick} />);
-  expect(screen.getByText('test')).toBeInTheDocument();
-  button.findAllByRole("button")
+  expect(screen.getByText("test")).toBeInTheDocument();
+  button.findAllByRole("button");
   fireEvent(
     screen.getByText("test"),
-    new MouseEvent('click', {
+    new MouseEvent("click", {
       bubbles: true,
       cancelable: true,
-    }),
-  )
+    })
+  );
   expect(onClick).toBeCalledTimes(1);
 });
